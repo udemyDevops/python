289bca17b6a4ee63d3ac7b4ba72f3847d0e0ed59
yeswanth <yswnth.2010@gmail.com>
2025-11-22 19:51:00 +0530

    python

--- Python-Scripts/python-syntaxx.py
+++ Python-Scripts/python-syntaxx.py
@@ -11,3 +11,11 @@ else:
   print("Value of x is non zero")
   print()
   print("This statement is out of if/else block.")
+
+
+# if x == 0:
+#   print("In the If Block. \nValue of x is 0 \n")
+# else:
+#   print("In the else block. \nValue of x is non zero \n")
+
+# print("This statement is out of if/else block.")
